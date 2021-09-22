2b64de193397a8e9a2625a1f6a04478018268b43
Edward Chen <18449977+edgchen1@users.noreply.github.com>
2021-09-22 10:37:36 -0700

    Add CI build for iOS Object Detection example (#30)

---
+++ mobile/examples/object_detection/ios/ORTObjectDetectionTests/ORTObjectDetectionTests.swift
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import XCTest
+
+@testable import ORTObjectDetection
+
+class ORTObjectDetectionTests: XCTestCase {
+  func testModelLoads() throws {
+    let modelHandler = ModelHandler(
+      modelFileInfo: (name: "ssd_mobilenet_v1.all", extension: "ort"),
+      labelsFileInfo: (name: "labelmap", extension: "txt"))
+    XCTAssertNotNil(modelHandler)
+  }
+}
