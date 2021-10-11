0e72601356b5acdf7d3897d828854ef435bf1266
Chi Lo <54722500+chilo-ms@users.noreply.github.com>
2021-10-11 13:37:32 -0700

    Add inference and accuracy evaluation for QDQ e2e example (#36)
    * Add inference and evaluation for BERT QDQ
    
    * Add fp16 and int8 flags
    
    * Modify data reader to handle multiple features
    
    * Set batch size to 1 due to some qdq model might have issues processing batch size greate than 1
    
    * Add comment and ignore last commit commit
    
    * Add evaluation for squad v2.0
    
    * Run evaluate script directly in e2e script

--- quantization/nlp/bert/trt/e2e_tensorrt_bert_example.py
+++ quantization/nlp/bert/trt/e2e_tensorrt_bert_example.py
@@ -11,62 +11,179 @@ import six
 import unicodedata
 import onnx
 import onnxruntime
+import sys
 import data_processing as dp
 import tokenization
 from pathlib import Path
+import subprocess
 from onnxruntime.quantization import CalibrationDataReader, create_calibrator, write_calibration_table, QuantType, QuantizationMode, QLinearOpsRegistry, QDQQuantizer
 
 class BertDataReader(CalibrationDataReader):
-    def __init__(self, model_path, squad_json, vocab_file, batch_size, max_seq_length, num_inputs):
+    def __init__(self, model_path, squad_json, vocab_file, batch_size, max_seq_length, num_inputs=None):
         self.model_path = model_path
         self.data = dp.read_squad_json(squad_json)
         self.max_seq_length = max_seq_length
         self.batch_size = batch_size
-        self.current_index = 0
-        self.num_inputs = num_inputs
+        self.example_stride = batch_size 
+        self.current_example_index = 0 # example index
+        self.current_feature_index = 0 # global feature index 
+        self.num_inputs = min(num_inputs, len(self.data)) if num_inputs else len(self.data)
         self.tokenizer = tokenization.BertTokenizer(vocab_file=vocab_file, do_lower_case=True)
         self.doc_stride = 128
         self.max_query_length = 64
         self.enum_data_dicts = iter([])
-        self.feature_list = []
+        self.features_list = []
         self.token_list = []
         self.example_id_list = []
+        self.start_of_new_stride = False
 
     def get_next(self):
         iter_data = next(self.enum_data_dicts, None)
         if iter_data:
+            self.start_of_new_stride= False
             return iter_data
 
         self.enum_data_dicts = None
-        if self.current_index + self.batch_size > self.num_inputs:
-            print("Calibrating index {:} batch size {:} exceed max input limit {:} sentences".format(self.current_index, self.batch_size, self.num_inputs))
+        if self.current_example_index >= self.num_inputs:
+            print("Reading dataset is done. Total examples is {:}".format(self.num_inputs))
             return None
+        elif self.current_example_index + self.example_stride > self.num_inputs:
+            self.example_stride = self.num_inputs - self.current_example_index
 
-        current_batch = int(self.current_index / self.batch_size)
-        if current_batch % 10 == 0:
-            print("Calibrating batch {:}, containing {:} sentences".format(current_batch, self.batch_size))
+        if self.current_example_index % 10 == 0:
+            current_batch = int(self.current_feature_index / self.batch_size) 
+            print("Reading example index {:}, batch {:}, containing {:} sentences".format(self.current_example_index, current_batch, self.batch_size))
 
-        input_ids = []
-        input_mask = []
-        segment_ids = []
-        for i in range(self.batch_size):
-            example = self.data[self.current_index + i]
+        # example could have more than one feature
+        # we collect all the features of examples and process them in one example stride
+        features_in_current_stride = []
+        for i in range(self.example_stride):
+            example = self.data[self.current_example_index+ i]
             features = dp.convert_example_to_features(example.doc_tokens, example.question_text, self.tokenizer, self.max_seq_length, self.doc_stride, self.max_query_length)
             self.example_id_list.append(example.id)
-            self.feature_list.append(features)
+            self.features_list.append(features)
             self.token_list.append(example.doc_tokens)
-            if len(input_ids) and len(segment_ids) and len(input_mask):
-                input_ids = np.vstack([input_ids, features[0].input_ids])
-                input_mask = np.vstack([input_mask, features[0].input_mask])
-                segment_ids = np.vstack([segment_ids, features[0].segment_ids])
-            else:
-                input_ids = np.expand_dims(features[0].input_ids, axis=0)
-                input_mask = np.expand_dims(features[0].input_mask, axis=0)
-                segment_ids = np.expand_dims(features[0].segment_ids, axis=0)
-        data = [{"input_ids": input_ids, "input_mask": input_mask, "segment_ids":segment_ids}]
-        self.current_index += self.batch_size
+            features_in_current_stride += features
+        self.current_example_index += self.example_stride
+        self.current_feature_index+= len(features_in_current_stride)
+
+
+        # following layout shows three examples as example stride with batch size 2:
+        # 
+        # start of new example stride 
+        # |
+        # |
+        # v
+        # <--------------------- batch size 2 ---------------------->
+        # |...example n, feature 1....||...example n, feature 2.....| 
+        # |...example n, feature 3....||...example n+1, feature 1...| 
+        # |...example n+1, feature 2..||...example n+1, feature 3...|
+        # |...example n+1, feature 4..||...example n+2, feature 1...|
+
+        data = []
+        for feature_idx in range(0, len(features_in_current_stride), self.batch_size):
+            input_ids = []
+            input_mask = []
+            segment_ids = []
+
+            for i in range(self.batch_size):
+                if feature_idx + i >= len(features_in_current_stride):
+                    break
+                feature = features_in_current_stride[feature_idx + i]
+                if len(input_ids) and len(segment_ids) and len(input_mask):
+                    input_ids = np.vstack([input_ids, feature.input_ids])
+                    input_mask = np.vstack([input_mask, feature.input_mask])
+                    segment_ids = np.vstack([segment_ids, feature.segment_ids])
+                else:
+                    input_ids = np.expand_dims(feature.input_ids, axis=0)
+                    input_mask = np.expand_dims(feature.input_mask, axis=0)
+                    segment_ids = np.expand_dims(feature.segment_ids, axis=0)
+
+            data.append({"input_ids": input_ids, "attention_mask": input_mask, "token_type_ids":segment_ids})
+            # data.append({"input_ids": input_ids, "input_mask": input_mask, "segment_ids":segment_ids})
+
         self.enum_data_dicts = iter(data)
+        self.start_of_new_stride = True
         return next(self.enum_data_dicts, None)
+
+def get_predictions(example_id_in_current_stride,
+                    features_in_current_stride,
+                    token_list_in_current_stride,
+                    batch_size,
+                    outputs,
+                    _NetworkOutput,
+                    all_predictions):
+                    
+    if example_id_in_current_stride == []:
+        return 
+
+    base_feature_idx = 0
+    for idx, id in enumerate(example_id_in_current_stride):
+        features = features_in_current_stride[idx]
+        doc_tokens = token_list_in_current_stride[idx]
+        networkOutputs = []
+        for i in range(len(features)):
+            x = (base_feature_idx + i) // batch_size
+            y = (base_feature_idx + i) % batch_size
+
+            output = outputs[x]
+            start_logits = output[0][y]
+            end_logits = output[1][y]
+
+            networkOutputs.append(_NetworkOutput(
+                start_logits = start_logits,
+                end_logits = end_logits,
+                feature_index = i 
+                ))
+
+        base_feature_idx += len(features) 
+
+        # Total number of n-best predictions to generate in the nbest_predictions.json output file
+        n_best_size = 20
+
+        # The maximum length of an answer that can be generated. This is needed
+        # because the start and end predictions are not conditioned on one another
+        max_answer_length = 30
+
+        prediction, nbest_json, scores_diff_json = dp.get_predictions(doc_tokens, features,
+                networkOutputs, n_best_size, max_answer_length)
+
+        all_predictions[id] = prediction
+
+def inference(data_reader, ort_session):
+
+    _NetworkOutput = collections.namedtuple(  # pylint: disable=invalid-name
+            "NetworkOutput",
+            ["start_logits", "end_logits", "feature_index"])
+    all_predictions = collections.OrderedDict()
+    
+    example_id_in_current_stride = [] 
+    features_in_current_stride = []  
+    token_list_in_current_stride = []
+    outputs = []
+    while True:
+        inputs = data_reader.get_next()
+        if not inputs:
+            break
+
+        if data_reader.start_of_new_stride:
+            get_predictions(example_id_in_current_stride, features_in_current_stride, token_list_in_current_stride, data_reader.batch_size, outputs, _NetworkOutput, all_predictions)
+
+            # reset current example stride
+            example_id_in_current_stride = data_reader.example_id_list[-data_reader.example_stride:]
+            features_in_current_stride = data_reader.features_list[-data_reader.example_stride:] 
+            token_list_in_current_stride = data_reader.token_list[-data_reader.example_stride:]
+            outputs = []
+
+        output = ort_session.run(["output_start_logits","output_end_logits"], inputs)
+        outputs.append(output)
+
+
+    # handle the last example stride
+    get_predictions(example_id_in_current_stride, features_in_current_stride, token_list_in_current_stride, data_reader.batch_size, outputs, _NetworkOutput, all_predictions)
+
+    return all_predictions
+
 
 if __name__ == '__main__':
     '''
@@ -79,19 +196,21 @@ if __name__ == '__main__':
     The onnx model used in the script is converted from Hugging Face BERT model,
     https://huggingface.co/transformers/serialization.html#converting-an-onnx-model-using-the-transformers-onnx-package
 
-    Some utility functions for dataset processing and data reader are from Nvidia TensorRT demo BERT repo,
+    Some utility functions for dataset processing, data reader and evaluation are from Nvidia TensorRT demo BERT repo,
     https://github.com/NVIDIA/TensorRT/tree/master/demo/BERT
     '''
 
     # Model, dataset and quantization settings
     model_path = "./model.onnx"
     squad_json = "./squad/dev-v1.1.json"
+    # squad_json = "./squad/dev-v2.0.json" # uncomment it if you want to use squad v2.0 as dataset
     vocab_file = "./squad/vocab.txt"
     augmented_model_path = "./augmented_model.onnx"
     qdq_model_path = "./qdq_model.onnx"
     sequence_lengths = [384]
     calib_num = 100
     op_types_to_quantize = ['MatMul', 'Transpose', 'Add']
+    batch_size = 1
 
     # Generate INT8 calibration cache
     print("Calibration starts ...")
@@ -99,7 +218,7 @@ if __name__ == '__main__':
         op_types_to_quantize = list(QLinearOpsRegistry.keys())
     calibrator = create_calibrator(model_path, op_types_to_quantize, augmented_model_path=augmented_model_path)
     calibrator.set_execution_providers(["CUDAExecutionProvider"]) 
-    data_reader = BertDataReader(model_path, squad_json, vocab_file, 2, sequence_lengths[-1], calib_num)
+    data_reader = BertDataReader(model_path, squad_json, vocab_file, batch_size, sequence_lengths[-1], calib_num)
     calibrator.collect_data(data_reader)
     compute_range = calibrator.compute_range()
     write_calibration_table(compute_range)
@@ -125,4 +244,27 @@ if __name__ == '__main__':
     quantizer.quantize_model()
     quantizer.model.save_model_to_file(qdq_model_path, False)
     print("QDQ model is saved to ", qdq_model_path)
-    
+
+    # QDQ model inference and get SQUAD prediction 
+    os.environ["ORT_TENSORRT_FP16_ENABLE"] = "1"  # Enable TRT FP16 precision
+    os.environ["ORT_TENSORRT_INT8_ENABLE"] = "1"  # Enable TRT INT8 precision
+    batch_size = 1 
+    data_reader = BertDataReader(qdq_model_path, squad_json, vocab_file, batch_size, sequence_lengths[-1])
+    sess_options = onnxruntime.SessionOptions()
+    sess_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_DISABLE_ALL
+    session = onnxruntime.InferenceSession(qdq_model_path, sess_options=sess_options, providers=["TensorrtExecutionProvider", "CUDAExecutionProvider"])
+    all_predictions = inference(data_reader, session) 
+
+    prediction_file = "./prediction.json"
+    with open(prediction_file, "w") as f:
+        f.write(json.dumps(all_predictions, indent=4))
+        print("\nOutput dump to {}".format(prediction_file))
+
+    print("Evaluate QDQ model for SQUAD v1.1")
+    subprocess.call(['python', './squad/evaluate-v1.1.py', './squad/dev-v1.1.json', './prediction.json', '90'])
+
+    # uncomment following code if you want to evaluate on SQUAD v2.0
+    # you also need to re-run QDQ model inference to get prediction based on dev-v2.0.json
+    #
+    # print("Evaluate QDQ model for SQUAD v2.0")
+    # subprocess.call(['python', './squad/evaluate-v2.0.py', './squad/dev-v2.0.json', './prediction.json'])
