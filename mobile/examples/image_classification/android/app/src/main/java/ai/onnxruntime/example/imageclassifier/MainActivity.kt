022feafb0e17f1f874bcb768b854577b20eb6753
Guangyun Han <guangyunhan@microsoft.com>
2022-03-07 16:43:30 +0800

    Use ORT format

--- mobile/examples/image_classification/android/app/src/main/java/ai/onnxruntime/example/imageclassifier/MainActivity.kt
+++ mobile/examples/image_classification/android/app/src/main/java/ai/onnxruntime/example/imageclassifier/MainActivity.kt
@@ -178,7 +178,7 @@ class MainActivity : AppCompatActivity() {
 
     // Read ort model into a ByteArray, run in background
     private suspend fun readModel(): ByteArray = withContext(Dispatchers.IO) {
-        val modelID = if (currentEp == "OpenCL") R.raw.mobilenet_v2_onnx else
+        val modelID = if (currentEp == "OpenCL") R.raw.mobilenet_v2_opencl_ort else
             if (enableQuantizedModel) R.raw.mobilenet_v2_uint8 else R.raw.mobilenet_v2_float
         resources.openRawResource(modelID).readBytes()
     }
