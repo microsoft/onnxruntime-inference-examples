ea6ea3748cc141c874685d97245c816b50a39c64
Nat Kershaw <nakersha@microsoft.com>
2022-04-08 18:34:16 -0700

    Debug for Azure

--- python/azureml/score.py
+++ python/azureml/score.py
@@ -11,14 +11,19 @@ doc_stride = 128
 max_query_length = 64
 
 def preprocess(question, context):
+    logging.info("Question:", question)
+    logging.info("Context: ", context)
     encoded_input = tokenizer(question, context)
     tokens = tokenizer.convert_ids_to_tokens(encoded_input.input_ids)
+    print(tokens)
     return (encoded_input.input_ids, encoded_input.token_type_ids, tokens)
 
 def postprocess(tokens, output):
     results = {}
     answer_start = np.argmax(output['start_logits'])
     answer_end = np.argmax(output['end_logits'])
+    print(answer_start)
+    print(answer_end)
     if answer_end >= answer_start:
         answer = tokens[answer_start]
         for i in range(answer_start+1, answer_end+1):
@@ -52,18 +57,20 @@ def init():
 
 def run(raw_data):
     logging.info("Request received")
-    input = json.loads(raw_data)
+    inputs = json.loads(raw_data)
+
+    logging.info(inputs)
 
     # Preprocess the question and context into tokenized ids
-    input_ids, segment_ids, tokens = preprocess(input["question"], input["context"])
+    input_ids, segment_ids, tokens = preprocess(inputs["question"], inputs["context"])
   
     # Format the inputs for ONNX Runtime
-    inputs = {
+    model_inputs = {
         'input_ids':   [input_ids], 
         'segment_ids': [segment_ids]
         }
                   
-    outputs = session.run(['start_logits', 'end_logits'], inputs)
+    outputs = session.run(['start_logits', 'end_logits'], model_inputs)
 
     # Format the outputs for the post processing function
     outputs_dict = {
@@ -76,5 +83,7 @@ def run(raw_data):
 
 
 if __name__ == '__main__':
-    init()
-    print(run("{\"question\": \"What is my name?\", \"context\": \"My name is Natalie\"}"))
+    init()   
+    input 
+    print(run("{\"question\": \"What is a major importance of Southern California in relation to California and the United States?\", \"context\": \"Southern California, often abbreviated SoCal, is a geographic and cultural region that generally comprises California\'s southernmost 10 counties. The region is traditionally described as \'eight counties\', based on demographics and economic ties: Imperial, Los Angeles, Orange, Riverside, San Bernardino, San Diego, Santa Barbara, and Ventura. The more extensive 10-county definition, including Kern and San Luis Obispo counties, is also used based on historical political divisions. Southern California is a major economic center for the state of California and the United States.\"}"))
+
