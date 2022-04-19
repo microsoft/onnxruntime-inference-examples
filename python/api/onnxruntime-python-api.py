48731774e28483a91d66fa36180ad7a5081692d9
Nat Kershaw <nakersha@microsoft.com>
2022-04-18 19:08:50 -0700

    Fix merge conflict.

--- python/api/onnxruntime-python-api.py
+++ python/api/onnxruntime-python-api.py
@@ -115,11 +115,7 @@ def main():
     print(run(x=np.float32([1.0, 2.0, 3.0]),y=np.float32([4.0, 5.0, 6.0])))
     # [array([5., 7., 9.], dtype=float32)]
 
-<<<<<<< HEAD
-    print(run_with_data_copied_to_device(x=np.float32([1.0, 2.0, 3.0, 4.0, 5.0]), y=np.float32([1.0, 2.0, 3.0, 4.0, 5.0])).numpy())
-=======
     print(run_with_data_on_device(x=np.float32([1.0, 2.0, 3.0, 4.0, 5.0]), y=np.float32([1.0, 2.0, 3.0, 4.0, 5.0])).numpy())
->>>>>>> 0bb1dca64b0848c67989c8cada957c95d374e83d
     # [ 2.  4.  6.  8. 10.]
 
     print(run_with_torch_tensors_on_device(torch.rand(5).to(DEVICE), torch.rand(5).to(DEVICE)))
