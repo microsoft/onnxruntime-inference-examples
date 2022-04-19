ca2253f12f3f043147a811c4d2e77d60f8e3179f
Nat Kershaw <nakersha@microsoft.com>
2022-04-18 18:56:39 -0700

    Add endpoint deployment using the CLI

--- python/azureml/score.py
+++ python/azureml/score.py
@@ -6,23 +6,23 @@ import onnxruntime
 import transformers
 import torch
 
-
+# The pre process function take a question and a context, and generates the tensor inputs to the model:
+# - input_ids: the words in the question encoded as integers
+# - attention_mask: not used in this model
+# - token_type_ids: a list of 0s and 1s that distinguish between the words of the question and the words of the context
+# This function also returns the words contained in the question and the context, so that the answer can be decoded into a phrase. 
 def preprocess(question, context):
-    print("Question:", question)
-    print("Context: ", context)
     encoded_input = tokenizer(question, context)
     tokens = tokenizer.convert_ids_to_tokens(encoded_input.input_ids)
-    print(tokens)
     return (encoded_input.input_ids, encoded_input.attention_mask, encoded_input.token_type_ids, tokens)
 
+# The post process function takes the list of tokens in the question and context, as well as the output of the 
+# model, the list of log probabilities for the choices of start and end of the answer, and maps it back to an
+# answer to the question that is asked of the context.
 def postprocess(tokens, start, end):
-    print("Start:", start)
-    print("End:", end)
     results = {}
     answer_start = np.argmax(start)
     answer_end = np.argmax(end)
-    print("Start: ", answer_start)
-    print("End: ", answer_end)
     if answer_end >= answer_start:
         answer = tokens[answer_start]
         for i in range(answer_start+1, answer_end+1):
@@ -35,6 +35,7 @@ def postprocess(tokens, start, end):
         results['error'] = "I am unable to find the answer to this question. Can you please ask another question?"
     return results
 
+# Perform the one-off intialization for the prediction. The init code is run once when the endpoint is setup.
 def init():
     global tokenizer, session, model
 
@@ -55,6 +56,7 @@ def init():
     session = onnxruntime.InferenceSession(model_path, providers=["CPUExecutionProvider"])  
 
 
+# Run the PyTorch model, for functional and performance comparison
 def run_pytorch(raw_data):
     inputs = json.loads(raw_data)
 
@@ -65,17 +67,14 @@ def run_pytorch(raw_data):
 
     input_ids, input_mask, segment_ids, tokens = preprocess(inputs["question"], inputs["context"])
 
-    print(input_ids)
-
     model_outputs = model(torch.tensor([input_ids]),  token_type_ids=torch.tensor([segment_ids]))
 
-    outputs = postprocess(tokens, model_outputs.start_logits.detach().numpy(), model_outputs.end_logits.detach().numpy())
+    return postprocess(tokens, model_outputs.start_logits.detach().numpy(), model_outputs.end_logits.detach().numpy())
 
-    print(outputs)
-
-
+# Run the ONNX model with ONNX Runtime
 def run(raw_data):
     logging.info("Request received")
+
     inputs = json.loads(raw_data)
 
     logging.info(inputs)
@@ -83,7 +82,7 @@ def run(raw_data):
     # Preprocess the question and context into tokenized ids
     input_ids, input_mask, segment_ids, tokens = preprocess(inputs["question"], inputs["context"])
 
-    print(input_ids)
+    logging.info("Running inference")
     
     # Format the inputs for ONNX Runtime
     model_inputs = {
@@ -93,17 +92,19 @@ def run(raw_data):
         }
                   
     outputs = session.run(['start_logits', 'end_logits'], model_inputs)
-    
+
+    logging.info("Post-processing")  
+
     # Post process the output of the model into an answer (or an error if the question could not be answered)
-    return postprocess(tokens, outputs[0], outputs[1])
+    results = postprocess(tokens, outputs[0], outputs[1])
+
+    logging.info(results)
+
+    return results
 
 
 if __name__ == '__main__':
     init()
-
-    #input = "{\"question\": \"What is my name?\", \"context\": \"My name is Natalie, and my sister's name is also Nathalie and my brother's name is dufas and my friend's name is Remy\"}"
-
-    #input = "{\"question\": \"What is Dolly Parton's middle name?\", \"context\": \"Dolly Rebecca Parton (born January 19, 1946) is an American singer-songwriter, actress, and businesswoman, known primarily for her work in country music. After achieving success as a songwriter for others, Parton made her album debut in 1967 with Hello, I'm Dolly, which led to success during the remainder of the 1960s (both as a solo artist and with a series of duet albums with Porter Wagoner), before her sales and chart peak came during the 1970s and continued into the 1980s. Parton's albums in the 1990s did not sell as well, but she achieved commercial success again in the new millennium and has released albums on various independent labels since 2000, including her own label, Dolly Records. She has sold more than 100 million records worldwide.\"}"
 
     input = "{\"question\": \"What is Dolly Parton's middle name?\", \"context\": \"Dolly Rebecca Parton is an American singer-songwriter\"}"
 
