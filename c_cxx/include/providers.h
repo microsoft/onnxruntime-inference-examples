124866d185c95f3f8729ec1a86c7c73f30b0dda3
Pranav Sharma <prs@microsoft.com>
2022-09-22 17:31:21 -0700

    Fix build errors

--- c_cxx/include/providers.h
+++ c_cxx/include/providers.h
@@ -2,23 +2,23 @@
 // Licensed under the MIT License.
 
 #pragma once
-#include "cpu_provider_factory.h"
+#include "cpu/cpu_provider_factory.h"
 
 #ifdef USE_CUDA
-#include "cuda_provider_factory.h"
+#include "cuda/cuda_provider_factory.h"
 #endif
 #ifdef USE_DNNL
-#include "dnnl_provider_factory.h"
+#include "dnnl/dnnl_provider_factory.h"
 #endif
 #ifdef USE_NUPHAR
-#include "nuphar_provider_factory.h"
+#include "nuphar/nuphar_provider_factory.h"
 #endif
 #ifdef USE_TENSORRT
-#include "tensorrt_provider_factory.h"
+#include "tensorrt/tensorrt_provider_factory.h"
 #endif
 #ifdef USE_DML
-#include "dml_provider_factory.h"
+#include "dml/dml_provider_factory.h"
 #endif
 #ifdef USE_MIGRAPHX
-#include "migraphx_provider_factory.h"
+#include "migraphx/migraphx_provider_factory.h"
 #endif
