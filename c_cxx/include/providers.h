d5151dc267f3ba3173d8ac5d6ba3d59c717f7482
Pranav Sharma <prs@microsoft.com>
2022-09-22 20:39:56 -0700

    Address review comment

--- c_cxx/include/providers.h
+++ c_cxx/include/providers.h
@@ -2,23 +2,23 @@
 // Licensed under the MIT License.
 
 #pragma once
-#include "cpu/cpu_provider_factory.h"
+#include "cpu_provider_factory.h"
 
 #ifdef USE_CUDA
-#include "cuda/cuda_provider_factory.h"
+#include "cuda_provider_factory.h"
 #endif
 #ifdef USE_DNNL
-#include "dnnl/dnnl_provider_factory.h"
+#include "dnnl_provider_factory.h"
 #endif
 #ifdef USE_NUPHAR
-#include "nuphar/nuphar_provider_factory.h"
+#include "nuphar_provider_factory.h"
 #endif
 #ifdef USE_TENSORRT
-#include "tensorrt/tensorrt_provider_factory.h"
+#include "tensorrt_provider_factory.h"
 #endif
 #ifdef USE_DML
-#include "dml/dml_provider_factory.h"
+#include "dml_provider_factory.h"
 #endif
 #ifdef USE_MIGRAPHX
-#include "migraphx/migraphx_provider_factory.h"
+#include "migraphx_provider_factory.h"
 #endif
