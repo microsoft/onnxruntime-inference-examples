562592a54603b5bf0886e9f1cf882e85d14c2229
Rachel Guo <guorachel@microsoft.com>
2023-02-14 13:46:39 -0800

    refine input image all moving to assets folder

--- mobile/examples/super_resolution/android/app/src/main/java/ai/onnxruntime/example/superresolution/MainActivity.kt
+++ mobile/examples/super_resolution/android/app/src/main/java/ai/onnxruntime/example/superresolution/MainActivity.kt
@@ -3,6 +3,7 @@ package ai.onnxruntime.example.superresolution
 import ai.onnxruntime.*
 import ai.onnxruntime.extensions.OrtxPackage
 import android.annotation.SuppressLint
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
@@ -13,13 +14,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import java.io.InputStream
 import java.util.*
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 
 class MainActivity : AppCompatActivity() {
     private var ortEnv: OrtEnvironment = OrtEnvironment.getEnvironment()
     private lateinit var ortSession: OrtSession
+    private var inputImage: ImageView? = null
     private var outputImage: ImageView? = null
     private var superResolutionButton: Button? = null
 
@@ -28,8 +28,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        outputImage = findViewById(R.id.imageView2);
+        inputImage = findViewById(R.id.imageView1)
+        outputImage = findViewById(R.id.imageView2)
         superResolutionButton = findViewById(R.id.super_resolution_button)
+        inputImage?.setImageBitmap(
+            BitmapFactory.decodeStream(readInputImage())
+        );
 
         // Initialize Ort Session and register the onnxruntime extensions package that contains the custom operators.
         // Note: These are used to decode the input image into the format the original model requires,
