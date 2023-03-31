ad71b4a7f1821fb1458966981fe4dbf924558885
Caio Vieira <caioxyz@hotmail.com>
2023-03-31 13:06:05 -0300

    Update model-explorer.cpp to current API
    The current model-explorer.cpp now builds correctly.

--- c_cxx/model-explorer/model-explorer.cpp
+++ c_cxx/model-explorer/model-explorer.cpp
@@ -11,7 +11,7 @@
  * Assumptions made in this example:
  *  1) The onnx model has 1 input node and 1 output node
  *
- * 
+ *
  * In this example, we do the following:
  *  1) read in an onnx model
  *  2) print out some metadata information about inputs and outputs that the model expects
@@ -21,17 +21,19 @@
  */
 
 #include <algorithm>  // std::generate
-#include <assert.h>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
 #include <iostream>
 #include <sstream>
+#include <string>
 #include <vector>
-#include <experimental_onnxruntime_cxx_api.h>
+#include <onnxruntime_cxx_api.h>
 
 // pretty prints a shape dimension vector
 std::string print_shape(const std::vector<int64_t>& v) {
   std::stringstream ss("");
-  for (size_t i = 0; i < v.size() - 1; i++)
-    ss << v[i] << "x";
+  for (size_t i = 0; i < v.size() - 1; i++) ss << v[i] << "x";
   ss << v[v.size() - 1];
   return ss.str();
 }
@@ -40,6 +42,14 @@ int calculate_product(const std::vector<int64_t>& v) {
   int total = 1;
   for (auto& i : v) total *= i;
   return total;
+}
+
+template <typename T>
+Ort::Value vec_to_tensor(std::vector<T>& data, const std::vector<int64_t>& shape) {
+  Ort::MemoryInfo mem_info =
+      Ort::MemoryInfo::CreateCpu(OrtAllocatorType::OrtArenaAllocator, OrtMemType::OrtMemTypeDefault);
+  auto tensor = Ort::Value::CreateTensor<T>(mem_info, data.data(), data.size(), shape.data(), shape.size());
+  return tensor;
 }
 
 using namespace std;
@@ -61,52 +71,63 @@ int main(int argc, char** argv) {
   // onnxruntime setup
   Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "example-model-explorer");
   Ort::SessionOptions session_options;
-  Ort::Experimental::Session session = Ort::Experimental::Session(env, model_file, session_options);  // access experimental components via the Experimental namespace
+  Ort::Session session = Ort::Session(env, model_file.c_str(), session_options);
 
   // print name/shape of inputs
-  std::vector<std::string> input_names = session.GetInputNames();
-  std::vector<std::vector<int64_t> > input_shapes = session.GetInputShapes();
-  cout << "Input Node Name/Shape (" << input_names.size() << "):" << endl;
-  for (size_t i = 0; i < input_names.size(); i++) {
-    cout << "\t" << input_names[i] << " : " << print_shape(input_shapes[i]) << endl;
+  Ort::AllocatorWithDefaultOptions allocator;
+  vector<string> input_names;
+  vector<int64_t> input_shapes;
+  std::cout << "Input Node Name/Shape (" << input_names.size() << "):" << std::endl;
+  for (std::size_t i = 0; i < session.GetInputCount(); i++) {
+    input_names.emplace_back(session.GetInputNameAllocated(i, allocator).get());
+    input_shapes = session.GetInputTypeInfo(i).GetTensorTypeAndShapeInfo().GetShape();
+    std::cout << "\t" << input_names.at(i) << " : " << print_shape(input_shapes) << std::endl;
   }
 
   // print name/shape of outputs
-  std::vector<std::string> output_names = session.GetOutputNames();
-  std::vector<std::vector<int64_t> > output_shapes = session.GetOutputShapes();
-  cout << "Output Node Name/Shape (" << output_names.size() << "):" << endl;
-  for (size_t i = 0; i < output_names.size(); i++) {
-    cout << "\t" << output_names[i] << " : " << print_shape(output_shapes[i]) << endl;
+  std::vector<std::string> output_names;
+  std::cout << "Output Node Name/Shape (" << output_names.size() << "):" << endl;
+  for (std::size_t i = 0; i < session.GetOutputCount(); i++) {
+    output_names.emplace_back(session.GetOutputNameAllocated(i, allocator).get());
+    auto output_shapes = session.GetOutputTypeInfo(i).GetTensorTypeAndShapeInfo().GetShape();
+    cout << "\t" << output_names.at(i) << " : " << print_shape(output_shapes) << endl;
   }
 
   // Assume model has 1 input node and 1 output node.
   assert(input_names.size() == 1 && output_names.size() == 1);
 
   // Create a single Ort tensor of random numbers
-  auto input_shape = input_shapes[0];
-  int total_number_elements = calculate_product(input_shape);
+  auto input_shape = input_shapes;
+  auto total_number_elements = calculate_product(input_shape);
+
+  // generate random numbers in the range [0, 255]
   std::vector<float> input_tensor_values(total_number_elements);
-  std::generate(input_tensor_values.begin(), input_tensor_values.end(), [&] { return rand() % 255; });  // generate random numbers in the range [0, 255]
+  std::generate(input_tensor_values.begin(), input_tensor_values.end(), [&] { return rand() % 255; });
   std::vector<Ort::Value> input_tensors;
-  input_tensors.push_back(Ort::Experimental::Value::CreateTensor<float>(input_tensor_values.data(), input_tensor_values.size(), input_shape));
+  input_tensors.emplace_back(vec_to_tensor<float>(input_tensor_values, input_shape));
 
   // double-check the dimensions of the input tensor
-  assert(input_tensors[0].IsTensor() &&
-         input_tensors[0].GetTensorTypeAndShapeInfo().GetShape() == input_shape);
+  assert(input_tensors[0].IsTensor() && input_tensors[0].GetTensorTypeAndShapeInfo().GetShape() == input_shape);
   cout << "\ninput_tensor shape: " << print_shape(input_tensors[0].GetTensorTypeAndShapeInfo().GetShape()) << endl;
 
   // pass data through model
-  cout << "Running model...";
+  std::vector<const char*> input_names_char(input_names.size(), nullptr);
+  std::transform(std::begin(input_names), std::end(input_names), std::begin(input_names_char),
+                 [&](const std::string& str) { return str.c_str(); });
+
+  vector<const char*> output_names_char(output_names.size(), nullptr);
+  std::transform(std::begin(output_names), std::end(output_names), std::begin(output_names_char),
+                 [&](const std::string& str) { return str.c_str(); });
+
+  cout << "Running model..." << std::endl;
   try {
-    auto output_tensors = session.Run(session.GetInputNames(), input_tensors, session.GetOutputNames());
-    cout << "done" << endl;
+    auto output_tensors = session.Run(Ort::RunOptions{nullptr}, input_names_char.data(), input_tensors.data(),
+                                      input_names_char.size(), output_names_char.data(), output_names_char.size());
+    cout << "Done!" << endl;
 
     // double-check the dimensions of the output tensors
     // NOTE: the number of output tensors is equal to the number of output nodes specifed in the Run() call
-    assert(output_tensors.size() == session.GetOutputNames().size() &&
-           output_tensors[0].IsTensor());
-    cout << "output_tensor_shape: " << print_shape(output_tensors[0].GetTensorTypeAndShapeInfo().GetShape()) << endl;
-
+    assert(output_tensors.size() == output_names.size() && output_tensors[0].IsTensor());
   } catch (const Ort::Exception& exception) {
     cout << "ERROR running model inference: " << exception.what() << endl;
     exit(-1);
