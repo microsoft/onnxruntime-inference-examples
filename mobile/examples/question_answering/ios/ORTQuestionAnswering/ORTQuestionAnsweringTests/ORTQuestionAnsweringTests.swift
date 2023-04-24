616ab59b71e1dc1199c5b309425e12522af56d2a
rachguo <rachguo@rachguos-Mac-mini.local>
2023-04-24 14:31:00 -0700

    update

--- mobile/examples/question_answering/ios/ORTQuestionAnswering/ORTQuestionAnsweringTests/ORTQuestionAnsweringTests.swift
+++ mobile/examples/question_answering/ios/ORTQuestionAnswering/ORTQuestionAnsweringTests/ORTQuestionAnsweringTests.swift
@@ -1,9 +1,5 @@
-//
-//  ORTQuestionAnsweringTests.swift
-//  ORTQuestionAnsweringTests
-//
-//  Created by rachguo on 4/24/23.
-//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 import XCTest
 
