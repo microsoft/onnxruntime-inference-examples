40ab4ff8aee6dca84646be5ed9e7e0d25def3851
Rachel Guo <35738743+YUNQIUGUO@users.noreply.github.com>
2023-04-24 18:13:24 -0700

    [iOS samples] Question Answering with ppp  (#224)
    * initial qa ios sample implementation
    
    * update
    
    * fix creating input tensor shape and attach a screenshot
    
    * add readme and clean up
    
    * add model to the repo for now
    
    * update
    
    * address pr comments
    
    * update
    
    * update
    
    * fix live updating user input in the app
    
    * update
    
    * add testing xcscheme for the app
    
    * update
    
    * add command to run tests in ci
    
    * fix directory
    
    * add updated project.pbxproj
    
    * update
    
    * minor updates
    
    ---------
    
    Co-authored-by: rachguo <rachguo@rachguos-Mini.attlocal.net>
    Co-authored-by: rachguo <rachguo@rachguos-Mac-mini.local>

---
+++ mobile/examples/question_answering/ios/ORTQuestionAnswering/ORTQuestionAnswering/ORTQuestionAnswering-Bridging-Header.h
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#ifndef ORTQuestionAnswering_Bridging_Header_h
+#define ORTQuestionAnswering_Bridging_Header_h
+
+#import "ORTQuestionAnsweringPerformer.h"
+
+#endif /* ORTQuestionAnswering_Bridging_Header_h */
