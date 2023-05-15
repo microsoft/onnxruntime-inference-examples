9d462eb160be76b1aa62d0efe5fdeb94dbc4c163
rachguo <rachguo@rachguos-Mac-mini.local>
2023-05-15 12:19:47 -0700

    update

--- mobile/examples/super_resolution/react_native/ort-super-resolution/App.tsx
+++ mobile/examples/super_resolution/react_native/ort-super-resolution/App.tsx
@@ -1,13 +1,13 @@
+import React from 'react';
 import { Buffer } from "buffer";
-import React, { useState } from 'react';
-import { Alert, Button, StyleSheet, Text, View, Image } from 'react-native';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
 import { Asset } from 'expo-asset';
 import { StatusBar } from 'expo-status-bar';
 import * as FileSystem from 'expo-file-system';
-
 import * as ort from 'onnxruntime-react-native';
 
 let base64js = require('base64-js')
+
 let myModel: ort.InferenceSession;
 
 const getUint8ArrayFromUri = async (uri: string): Promise<Uint8Array> => {
@@ -28,49 +28,59 @@ export default function App() {
 
   async function loadModel() {
     try {
+      // acquire and load input model
       const assets = await Asset.loadAsync(require('./assets/pytorch_superresolution_with_pre_post_processing_opset18.onnx'));
       const modelUri = assets[0].localUri;
       if (!modelUri) {
-        Alert.alert('failed to get model URI', `${assets[0]}`);
+        console.log('failed to get model URI', `${assets[0]}`);
       } else {
         myModel = await ort.InferenceSession.create(modelUri);
-        Alert.alert(
+        console.log(
           'model loaded successfully',
           `input names: ${myModel.inputNames}, output names: ${myModel.outputNames}`);
       }
     } catch (e) {
-      Alert.alert('failed to load model', `${e}`);
+      console.error('failed to load model', `${e}`);
       throw e;
     }
   }
 
   async function runModel() {
     try {
-      const feeds: Record<string, ort.Tensor> = {};
+      // Step 1: Prepare input image
       const assets = await Asset.loadAsync(require('./assets/cat_224x224.png'));
       let myImage = assets[0].localUri as string;
+
+      // Step 2: Prepare model inputs
+      const feeds: Record<string, ort.Tensor> = {};
       const imageUint8ArrayData = await getUint8ArrayFromUri(myImage);
       const dataLength = imageUint8ArrayData.length;
       feeds[myModel.inputNames[0]] = new ort.Tensor(imageUint8ArrayData, [dataLength]);
-      const fetches = await myModel.run(feeds);
-      const output = fetches[myModel.outputNames[0]];    // output pixelbuffer
 
+      // Step 3: Run ort inference
+      const fetches = await myModel.run(feeds);
+
+      // Step 4: Process output pixelbuffer and display as a png image on screen
+      const output = fetches[myModel.outputNames[0]];
       let encodedString = base64js.fromByteArray(output.data);
-      console.log(encodedString);
 
       setOutputImage({ localUri: `data:image/png;base64,${encodedString}` })
 
       if (!output) {
-        Alert.alert('failed to get output', `${myModel.outputNames[0]}`);
+        console.log('failed to get output', `${myModel.outputNames[0]}`);
       } else {
-        Alert.alert('model inference successfully');
+        console.log('model inference successfully');
       }
     } catch (e) {
-      Alert.alert('failed to inference model', `${e}`);
+      console.error('failed to inference model', `${e}`);
       throw e;
     }
   }
 
+  async function performSuperResolution() {
+    await loadModel();
+    await runModel();
+  }
 
   return (
     <View style={styles.container}>
@@ -80,8 +90,7 @@ export default function App() {
         style={styles.image}
         resizeMode="contain"></Image>
       <View>
-        <Button title='Load model' onPress={loadModel}></Button>
-        <Button title='Run' onPress={runModel}></Button>
+        <Button title='Perform Super Resolution' onPress={performSuperResolution}></Button>
       </View>
       <View>
         {
