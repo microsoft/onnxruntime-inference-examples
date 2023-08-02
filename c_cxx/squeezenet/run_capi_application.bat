f02c822393fcaed4b06a73481cbbb9eb8d559ed1
Yi Zhang <zhanyi@microsoft.com>
2023-08-02 10:59:59 +0800

    get msbuild path

--- c_cxx/squeezenet/run_capi_application.bat
+++ c_cxx/squeezenet/run_capi_application.bat
@@ -17,7 +17,10 @@ REM Copy ORT libraries to same folder for linker to build.
 REM For some reasons, setting "LINK" or "LIBPATH" env variables won't help. 
 cd build
 powershell -Command "cp %ORT_LIB%\* ."
-"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\amd64\MSBuild.exe" .\capi_test.sln /property:Configuration=Release
+for /f "tokens=*" %%a in ('"C:\\Program Files (x86)\\Microsoft Visual Studio\\Installer\\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\\**\\Bin\\MSBuild.exe') do (
+    set MSBUILD_PATH=%%a
+)
+"%MSBUILD_PATH%" .\capi_test.sln /property:Configuration=Release
 
 REM Copy ORT libraries to same folder for executable to run.
 cd Release
