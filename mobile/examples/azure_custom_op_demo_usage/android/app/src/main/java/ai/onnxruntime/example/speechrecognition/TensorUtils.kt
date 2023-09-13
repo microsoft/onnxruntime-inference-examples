92bea1f5a23cdb2c7703152167064c0e6358d540
rachguo <rachguo@rachguos-Mini.attlocal.net>
2023-09-13 11:50:23 -0700

    restructure

--- mobile/examples/azure_custom_op_demo_usage/android/app/src/main/java/ai/onnxruntime/example/speechrecognition/TensorUtils.kt
+++
@@ -1,3 +0,0 @@
-package ai.onnxruntime.example.speechrecognition
-
-internal fun tensorShape(vararg dims: Long) = longArrayOf(*dims)
---
+++ mobile/examples/whisper/android/azure/app/src/main/java/ai/onnxruntime/example/speechrecognition/TensorUtils.kt
@@ -0,0 +1,5 @@
+package ai.onnxruntime.example.speechrecognition
+
+// TODO: Add more tensor helper methods here if needed.
+
+internal fun tensorShape(vararg dims: Long) = longArrayOf(*dims)
