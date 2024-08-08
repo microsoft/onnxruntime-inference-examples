761c1c49bbb94624133a8730189fdacc3486769b
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2024-08-08 12:32:38 -0700

    update prepare_model.sh to use new model download URL.

--- mobile/examples/object_detection/ios/ORTObjectDetection/prepare_model.sh
+++ mobile/examples/object_detection/ios/ORTObjectDetection/prepare_model.sh
@@ -2,27 +2,28 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 # ============================================================
-set -e
+set -e -x
 
-MODELS_URL="https://tfhub.dev/tensorflow/lite-model/ssd_mobilenet_v1/1/metadata/1?lite-format=tflite"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 DOWNLOAD_DIR="${SCRIPT_DIR}/ModelsAndData"
 
-mkdir -p ${DOWNLOAD_DIR}
+mkdir -p "${DOWNLOAD_DIR}/tflite_model"
 
-#Download source tflite model file
-curl -L ${MODELS_URL} >${DOWNLOAD_DIR}/ssd_mobilenet_v1_1_metadata_1.tflite
+# Download source tflite model file
+curl -L -o "${DOWNLOAD_DIR}/tflite_model/model.tar.gz" \
+  https://www.kaggle.com/api/v1/models/tensorflow/ssd-mobilenet-v1/tfLite/metadata/1/download
 
-#Unzip and get model metadata
-unzip ${DOWNLOAD_DIR}/ssd_mobilenet_v1_1_metadata_1.tflite -d ${DOWNLOAD_DIR}
+# Extract model
+tar -xzf "${DOWNLOAD_DIR}/tflite_model/model.tar.gz" -C "${DOWNLOAD_DIR}/tflite_model"
 
-#Convert tflite model to onnx model
-python3 -m tf2onnx.convert --tflite ${DOWNLOAD_DIR}/ssd_mobilenet_v1_1_metadata_1.tflite --opset 13 --output ${DOWNLOAD_DIR}/ssd_mobilenet_v1.onnx
+# Convert tflite model to onnx model
+python -m tf2onnx.convert --tflite "${DOWNLOAD_DIR}/tflite_model/1.tflite" --opset 13 --output "${DOWNLOAD_DIR}/ssd_mobilenet_v1.onnx"
 
-#Convert onnx model to ort format
-#This step generates a .ort format ssd mobilenet model in /ModelsAndData.
-#The .ort format model is the one gets executed in this sample application
-python3 -m onnxruntime.tools.convert_onnx_models_to_ort ${DOWNLOAD_DIR}
+# Convert onnx model to ort format
+# This step generates a .ort format ssd mobilenet model in /ModelsAndData.
+# The .ort format model is the one gets executed in this sample application
+python -m onnxruntime.tools.convert_onnx_models_to_ort "${DOWNLOAD_DIR}/ssd_mobilenet_v1.onnx"
 
-#Remove the tflite model
-rm ${DOWNLOAD_DIR}/ssd_mobilenet_v1_1_metadata_1.tflite
+# Remove the tflite model
+rm -r "${DOWNLOAD_DIR}/tflite_model"
