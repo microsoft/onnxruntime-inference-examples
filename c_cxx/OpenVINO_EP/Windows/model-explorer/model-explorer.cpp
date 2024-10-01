0b8d7f9f854d3b739ae0cf82764f3f83f4bbc9e9
vthaniel <vishnudas.thaniel.s@intel.com>
2024-10-01 03:27:21 -0700

    Modified samples with changes in device_type and updated README instructions

--- c_cxx/OpenVINO_EP/Windows/model-explorer/model-explorer.cpp
+++ c_cxx/OpenVINO_EP/Windows/model-explorer/model-explorer.cpp
@@ -65,7 +65,7 @@ int main(int argc, char** argv) {
   #ifdef USE_OPENVINO
     // Using OPENVINO backend
     OrtOpenVINOProviderOptions options;
-    options.device_type = "CPU_FP32"; //Other options are: GPU_FP32, GPU_FP16, MYRIAD_FP16
+    options.device_type = "CPU"; 
     std::cout << "OpenVINO device type is set to: " << options.device_type << std::endl;
     session_options.AppendExecutionProvider_OpenVINO(options);
   #endif
