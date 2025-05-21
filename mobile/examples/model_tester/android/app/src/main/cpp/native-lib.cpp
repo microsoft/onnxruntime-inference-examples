0c70b9f65490cc596917284e5d4a11d818084a72
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2025-05-21 16:52:48 -0700

    add support for ep type and options args in jni function

--- mobile/examples/model_tester/android/app/src/main/cpp/native-lib.cpp
+++ mobile/examples/model_tester/android/app/src/main/cpp/native-lib.cpp
@@ -39,7 +39,7 @@ std::vector<std::string> JstringArrayToStdStrings(JNIEnv& env, jobjectArray jobj
   for (jsize i = 0; i < length; ++i) {
     const auto jobj = env.GetObjectArrayElement(jobjs, i);
     if (!env.IsInstanceOf(jobj, java_string_class)) {
-      throw std::runtime_error("jobjectArray element is not a string");
+      throw std::runtime_error("jobjectArray element is not a string.");
     }
     const auto jstr = static_cast<jstring>(jobj);
     strs.emplace_back(JstringToStdString(env, jstr));
@@ -69,9 +69,9 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_onnxruntime_example_modeltester_MainActivity_run(JNIEnv* env, jobject thiz,
                                                           jbyteArray java_model_bytes,
                                                           jint num_iterations,
-                                                          jstring execution_provider_type,
-                                                          jobjectArray execution_provider_option_names,
-                                                          jobjectArray execution_provider_option_values) {
+                                                          jstring java_execution_provider_type,
+                                                          jobjectArray java_execution_provider_option_names,
+                                                          jobjectArray java_execution_provider_option_values) {
   try {
     auto model_bytes = util::MakeUniqueJbyteArrayElementsPtr(*env, java_model_bytes);
     const size_t model_bytes_length = env->GetArrayLength(java_model_bytes);
@@ -82,7 +82,22 @@ Java_com_onnxruntime_example_modeltester_MainActivity_run(JNIEnv* env, jobject t
     config.model_path_or_bytes = model_bytes_span;
     config.num_iterations = num_iterations;
 
-    // TODO handle EP type and EP options
+    if (java_execution_provider_type != nullptr) {
+      config.ep.emplace();
+      config.ep->provider_name = util::JstringToStdString(*env, java_execution_provider_type);
+
+      if (java_execution_provider_option_names != nullptr &&
+          java_execution_provider_option_values != nullptr) {
+        const auto option_names = util::JstringArrayToStdStrings(*env, java_execution_provider_option_names);
+        const auto option_values = util::JstringArrayToStdStrings(*env, java_execution_provider_option_values);
+        if (option_names.size() != option_values.size()) {
+          throw std::runtime_error("Execution provider option names and values must have the same size.");
+        }
+        for (size_t i = 0; i < option_names.size(); ++i) {
+          config.ep->provider_options.emplace(option_names[i], option_values[i]);
+        }
+      }
+    }
 
     auto result = model_runner::Run(config);
 
