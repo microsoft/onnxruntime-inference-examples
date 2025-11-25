d165c2fdd66bc53e04f114603085190ce56d57f4
edgchen1 <18449977+edgchen1@users.noreply.github.com>
2025-11-25 10:49:27 -0800

    add comments about deps

--- plugin_execution_providers/basic/android/basicpluginepusage/build.gradle.kts
+++ plugin_execution_providers/basic/android/basicpluginepusage/build.gradle.kts
@@ -42,6 +42,15 @@ android {
 }
 
 dependencies {
+    // Plugin EP library module dependency
+    implementation(project(":basicpluginep"))
+
+    // onnxruntime-android dependency
+    // Note: Typically, we would want to use a release version, but some of the plugin EP
+    // infrastructure is not available in a released version yet. We'll depend on a dev version.
+    //implementation("com.microsoft.onnxruntime:onnxruntime-android:latest.release")
+    implementation(files("lib/onnxruntime-android-1.24.0-dev+commit-75d35474.aar"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -50,8 +59,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(project(":basicpluginep"))
-    implementation(files("lib/onnxruntime-android-1.24.0-dev+commit-75d35474.aar"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
